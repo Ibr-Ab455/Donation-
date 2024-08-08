0c1ca653d6674ef5aded7c2a78a469487af18e24
Ibrahim-ali <Khaliilali455@gmail.com>
2024-08-08 14:25:52 +0200

    create interface

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,38 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+function Register() {
+  return (
+    <div className='w-full h-screen'>
+      <div className='form-content flex items-center justify-center py-10'>
+         <form className='flex flex-col gap-4 w-[40%] p-6 bg-[#010100]'>
+            <h1 className='text-white text-2xl'>Register you account</h1>
+            <hr/>
+            <div className='flex flex-col gap-1'>
+            <label className='text-sm text-gray-500'>Full Name</label>
+             <input type='text' placeholder='Ibrahim Ahmed' className='w-[80%] border-1 rounded border-gray-500 outline-none p-2'/>
+            </div>
+
+            <div className='flex flex-col gap-1'>
+            <label className='text-sm text-gray-500'>Email</label>
+             <input type='email' placeholder='Example.com' className='w-[80%] border-1 rounded border-gray-500 outline-none p-2'/>
+            </div>
+
+            <div className='flex flex-col gap-1'>
+            <label className='text-sm text-gray-500'>Password</label>
+             <input type='password' placeholder='********' className='w-[80%] border-1 rounded border-gray-500 outline-none p-2'/>
+            </div>
+            
+            <button className='bg-[#fc2e20] w-[80%] rounded p-2 text-white text-xl cursor-pointer font-serif font-normal'>Register</button>
+            <div>
+                <span className='text-white'>Have an account ? <Link to="/login" className='text-blue-600'>Login</Link></span>
+            </div>
+         </form>
+        </div>
+    </div>
+  )
+}
+
+export default Register
